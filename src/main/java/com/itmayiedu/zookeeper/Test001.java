e3bd7371b9eeb2b43d029de086e3b0a281871944
fei <liufeiio5@163.com>
2018-08-28 01:34:01 +0800

    张三又改了0133

--- src/main/java/com/itmayiedu/zookeeper/Test001.java
+++ src/main/java/com/itmayiedu/zookeeper/Test001.java
@@ -35,7 +35,7 @@ public class Test001 {
 
 						System.out.println("zk 启动连接...");
 						System.out.println("我根本不想理你");
-						System.out.println("不知道怎么回事，最近想吐");
+						System.out.println("不知道怎么回事");
 
 						countDownLatch.countDown();
 						System.out.println("你以为我想理你啊");

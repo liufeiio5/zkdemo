487d0b0ed7af53ebab7cddc5ffc7050d71da8edb
fei <liufeiio5@163.com>
2019-03-27 15:41:30 +0800

    用测试类测试

--- springboot-zk-consumer/src/test/java/com/fei/zkdemo/springbootzkconsumer/SpringbootZkConsumerApplicationTests.java
+++ springboot-zk-consumer/src/test/java/com/fei/zkdemo/springbootzkconsumer/SpringbootZkConsumerApplicationTests.java
@@ -1,7 +1,10 @@
 package com.fei.zkdemo.springbootzkconsumer;
 
+import com.fei.zkdemo.springbootzkconsumer.service.UserService;
+import com.fei.zkdemo.springbootzkproducter.service.OrderService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +12,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SpringbootZkConsumerApplicationTests {
 
+    @Autowired
+    UserService userService;
+
     @Test
-    public void contextLoads() {
+    public void testZkCnnection() {
+        System.out.println(userService.showOrder());
     }
 
 }
